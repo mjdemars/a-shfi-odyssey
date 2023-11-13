d7447bfc5164a682442db99c8470012de94ad057
mjdemars <maddalendemars@gmail.com>
2023-11-13 16:54:16 -0500

    Added player movement

--- A Shfi Odyssey/Assets/Scripts/PlayerMovement.cs
+++ A Shfi Odyssey/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
+    public int maxJumpCount;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isJumping = false;
+    private bool isGrounded;
+    private int jumpCount;
+
+    // awake is called after all objects are initialized. called in a random order.
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        jumpCount = maxJumpCount;
+    }
+
+    // update is called once per frame, is good for input methods
     void Update()
     {
-        
+        //get inputs
+        ProcessInputs();
+
+        //animate
+        Animate();
+    }
+
+    // better for handling physics, can be called multiple times per update frame
+    private void FixedUpdate()
+    {
+        // check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+        if (isGrounded)
+        {
+            jumpCount = maxJumpCount;
+        }
+
+        //move
+        Move();
+    }
+
+    private void ProcessInputs() 
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void Animate() 
+    {
+        if (moveDirection > 0 && facingRight) {
+            FlipCharacter();
+        } else if (moveDirection < 0 && !facingRight) {
+            FlipCharacter();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping && jumpCount > 0)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jumpCount--;
+        }
+        isJumping = false;
+    }
+
+    private void FlipCharacter() {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
