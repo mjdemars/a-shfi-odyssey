20eac9a7fb75cfed95e9e6312a0fb53c07c944e0
sydbalcom <sbalcom3@gatech.edu>
2023-12-04 19:22:08 -0500

    Fix radius for rock gravity checks

--- A Shfi Odyssey/Assets/Scripts/ManageGridElement.cs
+++ A Shfi Odyssey/Assets/Scripts/ManageGridElement.cs
@@ -116,10 +116,10 @@ public class ManageGridElement : MonoBehaviour
             }
         }
         // if the space below a rock is empty, move down one space (gravity)
-        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .5f, whatStopsMovement)
-            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .5f, Player)
-            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .5f, Pushable)
-            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .5f, Anchor))
+        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .05f, whatStopsMovement)
+            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .05f, Player)
+            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .05f, Pushable)
+            && !Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .05f, Anchor))
         {
             // modify move point
             movePoint.position += new Vector3(0f, -1f, 0f);
